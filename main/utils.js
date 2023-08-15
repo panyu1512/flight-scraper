ab570c0755727359cec82579e9b35cd7e9adabac
panyu1512 <kikeferreragius1997@gmail.com>
2023-08-15 16:36:50 +0200

    update content to scrappe

--- main/utils.js
+++ main/utils.js
@@ -8,15 +8,15 @@ function getFlightOffers(data){
     for (let index = 0; index < data.length;) {    
       if (data[index + 1].includes('€')) {
         currentFlight = {
-          "outbound flight": data[index],
+          "outbound flight": data[index] + ' PMI-OPO',
           "return flight": null,
           "price": data[index + 1]
         };
         index += 2;
       } else {
         currentFlight = {
-          "outbound flight": data[index],
-          "return flight": data[index + 1],
+          "outbound flight": data[index] + ' PMI-OPO',
+          "return flight": data[index + 1] + ' OPO-PMI' ,
           "price": data[index + 2]
         };
         index += 3;
